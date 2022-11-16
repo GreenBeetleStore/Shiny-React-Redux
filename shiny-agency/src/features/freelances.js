28cd66f2f5b869aed990af1de2558aa06227e59c
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-11-16 15:10:56 +0100

    immer

--- shiny-agency/src/features/freelances.js
+++ shiny-agency/src/features/freelances.js
@@ -1,13 +1,14 @@
 import produce from 'immer'
+import { selectFreelances } from '../utils/selectors'
 
 // Le state initial de la feature freelances
 const initialState = {
-  // le statut permet de suivre l'état de la requête
-  status: 'void',
-  // les données lorsque la requête a fonctionné
-  data: null,
-  // l'erreur lorsque la requête échoue
-  error: null,
+   // le statut permet de suivre l'état de la requête
+   status: 'void',
+   // les données lorsque la requête a fonctionné
+   data: null,
+   // l'erreur lorsque la requête échoue
+   error: null,
 }
 // Les noms des actions
 const FETCHING = 'freelances/fetching'
@@ -20,64 +21,79 @@ const freelancesResolved = (data) => ({ type: RESOLVED, payload: data })
 // la requête a échoué
 const freelancesRejected = (error) => ({ type: REJECTED, payload: error })
 
+export async function fetchOrUpdateFreelances(store) {
+   const status = selectFreelances(store.getState()).freelances.status
+   if (status === 'pending' || status === 'updating') {
+      return
+   }
+   store.dispatch(freelancesFetching())
+   try {
+      const response = await fetch('http://localhost:8000/freelances')
+      const data = await response.json()
+      store.dispatch(freelancesResolved(data))
+   } catch (error) {
+      store.dispatch(freelancesRejected(error))
+   }
+}
+
 export default function freelancesReducer(state = initialState, action) {
-  // on utilise immer pour changer le state
-  return produce(state, (draft) => {
-    // on fait un switch sur le type de l'action
-    switch (action.type) {
-      // si l'action est de type FETCHING
-      case FETCHING: {
-        // si le statut est void
-        if (draft.status === 'void') {
-          // on passe en pending
-          draft.status = 'pending'
-          return
-        }
-        // si le statut est rejected
-        if (draft.status === 'rejected') {
-          // on supprime l'erreur et on passe en pending
-          draft.error = null
-          draft.status = 'pending'
-          return
-        }
-        // si le statut est resolved
-        if (draft.status === 'resolved') {
-          // on passe en updating (requête en cours mais des données sont déjà présentent)
-          draft.status = 'updating'
-          return
-        }
-        // sinon l'action est ignorée
-        return
+   // on utilise immer pour changer le state
+   return produce(state, (draft) => {
+      // on fait un switch sur le type de l'action
+      switch (action.type) {
+         // si l'action est de type FETCHING
+         case FETCHING: {
+            // si le statut est void
+            if (draft.status === 'void') {
+               // on passe en pending
+               draft.status = 'pending'
+               return
+            }
+            // si le statut est rejected
+            if (draft.status === 'rejected') {
+               // on supprime l'erreur et on passe en pending
+               draft.error = null
+               draft.status = 'pending'
+               return
+            }
+            // si le statut est resolved
+            if (draft.status === 'resolved') {
+               // on passe en updating (requête en cours mais des données sont déjà présentent)
+               draft.status = 'updating'
+               return
+            }
+            // sinon l'action est ignorée
+            return
+         }
+         // si l'action est de type RESOLVED
+         case RESOLVED: {
+            // si la requête est en cours
+            if (draft.status === 'pending' || draft.status === 'updating') {
+               // on passe en resolved et on sauvegarde les données
+               draft.data = action.payload
+               draft.status = 'resolved'
+               return
+            }
+            // sinon l'action est ignorée
+            return
+         }
+         // si l'action est de type REJECTED
+         case REJECTED: {
+            // si la requête est en cours
+            if (draft.status === 'pending' || draft.status === 'updating') {
+               // on passe en rejected, on sauvegarde l'erreur et on supprime les données
+               draft.status = 'rejected'
+               draft.error = action.payload
+               draft.data = null
+               return
+            }
+            // sinon l'action est ignorée
+            return
+         }
+         // Sinon (action invalide ou initialisation)
+         default:
+            // on ne fait rien (retourne le state sans modifications)
+            return
       }
-      // si l'action est de type RESOLVED
-      case RESOLVED: {
-        // si la requête est en cours
-        if (draft.status === 'pending' || draft.status === 'updating') {
-          // on passe en resolved et on sauvegarde les données
-          draft.data = action.payload
-          draft.status = 'resolved'
-          return
-        }
-        // sinon l'action est ignorée
-        return
-      }
-      // si l'action est de type REJECTED
-      case REJECTED: {
-        // si la requête est en cours
-        if (draft.status === 'pending' || draft.status === 'updating') {
-          // on passe en rejected, on sauvegarde l'erreur et on supprime les données
-          draft.status = 'rejected'
-          draft.error = action.payload
-          draft.data = null
-          return
-        }
-        // sinon l'action est ignorée
-        return
-      }
-      // Sinon (action invalide ou initialisation)
-      default:
-        // on ne fait rien (retourne le state sans modifications)
-        return
-    }
-  })
+   })
 }
--- shiny-agency/src/pages/Freelances/index.jsx
+++ shiny-agency/src/pages/Freelances/index.jsx
@@ -3,78 +3,88 @@ import { Link } from 'react-router-dom'
 import Card from '../../components/Card'
 import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
-import { useFetch } from '../../utils/hooks'
-import { useSelector } from 'react-redux'
-import { selectTheme } from '../../utils/selectors'
+import { useSelector, useStore } from 'react-redux'
+import { selectFreelances, selectTheme } from '../../utils/selectors'
+import { fetchOrUpdateFreelances } from '../../features/freelances'
+import { useEffect } from 'react'
 
 const CardsContainer = styled.div`
-  display: grid;
-  gap: 24px;
-  grid-template-rows: 350px 350px;
-  grid-template-columns: repeat(2, 1fr);
-  align-items: center;
-  justify-items: center;
+   display: grid;
+   gap: 24px;
+   grid-template-rows: 350px 350px;
+   grid-template-columns: repeat(2, 1fr);
+   align-items: center;
+   justify-items: center;
 `
 
 const PageTitle = styled.h1`
-  font-size: 30px;
-  text-align: center;
-  padding-bottom: 30px;
-  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+   font-size: 30px;
+   text-align: center;
+   padding-bottom: 30px;
+   color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const PageSubtitle = styled.h2`
-  font-size: 20px;
-  color: ${colors.secondary};
-  font-weight: 300;
-  text-align: center;
-  padding-bottom: 30px;
-  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+   font-size: 20px;
+   color: ${colors.secondary};
+   font-weight: 300;
+   text-align: center;
+   padding-bottom: 30px;
+   color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const LoaderWrapper = styled.div`
-  display: flex;
-  justify-content: center;
+   display: flex;
+   justify-content: center;
 `
 
 function Freelances() {
-  const theme = useSelector(selectTheme)
-  const { data, isLoading, error } = useFetch(
-    `http://localhost:8000/freelances`
-  )
+   const theme = useSelector(selectTheme)
+   const freelances = useSelector(selectFreelances)
 
-  const freelancersList = data?.freelancersList
+   const freelancersList = freelances.data?.freelancersList
 
-  if (error) {
-    return <span>Il y a un problème</span>
-  }
+   const store = useStore()
 
-  return (
-    <div>
-      <PageTitle theme={theme}>Trouvez votre prestataire</PageTitle>
-      <PageSubtitle theme={theme}>
-        Chez Shiny nous réunissons les meilleurs profils pour vous.
-      </PageSubtitle>
-      {isLoading ? (
-        <LoaderWrapper>
-          <Loader theme={theme} data-testid="loader" />
-        </LoaderWrapper>
-      ) : (
-        <CardsContainer>
-          {freelancersList?.map((profile) => (
-            <Link key={`freelance-${profile.id}`} to={`/profile/${profile.id}`}>
-              <Card
-                label={profile.job}
-                title={profile.name}
-                picture={profile.picture}
-                theme={theme}
-              />
-            </Link>
-          ))}
-        </CardsContainer>
-      )}
-    </div>
-  )
+   useEffect(() => {
+      fetchOrUpdateFreelances(store)
+   }, [store])
+
+   if (freelances.status === 'rejected') {
+      return <span>Il y a un problème</span>
+   }
+
+   const isLoading = freelances.status === 'void' || freelances.status === 'pending'
+
+   return (
+      <div>
+         <PageTitle theme={theme}>Trouvez votre prestataire</PageTitle>
+         <PageSubtitle theme={theme}>
+            Chez Shiny nous réunissons les meilleurs profils pour vous.
+         </PageSubtitle>
+         {isLoading ? (
+            <LoaderWrapper>
+               <Loader theme={theme} data-testid="loader" />
+            </LoaderWrapper>
+         ) : (
+            <CardsContainer>
+               {freelancersList?.map((profile) => (
+                  <Link
+                     key={`freelance-${profile.id}`}
+                     to={`/profile/${profile.id}`}
+                  >
+                     <Card
+                        label={profile.job}
+                        title={profile.name}
+                        picture={profile.picture}
+                        theme={theme}
+                     />
+                  </Link>
+               ))}
+            </CardsContainer>
+         )}
+      </div>
+   )
 }
 
 export default Freelances
